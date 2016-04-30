7347c3e639a355ce6e53d357bae55e6fcef15960
4loops <colpeterson@gmail.com>
2016-04-29 21:23:41 -0700

    Corrected bug in compatability vector. Added min_score heuristic.

--- generate_full_ciphers.py
+++ generate_full_ciphers.py
@@ -2,7 +2,7 @@ import cPickle as pickle
 import numpy as np
 
 ##primaries = pickle.load(open('partials_unigram_min.pickle','rb'))
-unigrams  = pickle.load(open('unigram_500000_sorted_list.pickle','rb'))
+#unigrams  = pickle.load(open('unigram_500000_sorted_list.pickle','rb'))
 #primaries = pickle.load(open('100000_words_partials_unigram_min.pickle','rb'))
 ##
 #
@@ -11,8 +11,8 @@ unigrams  = pickle.load(open('unigram_500000_sorted_list.pickle','rb'))
 #primaries = primaries[:1024*256]
 #print 'sorted. saving list'
 #pickle.dump(primaries,open('1024_256_primaries_sorted_list.pickle','wb'))
-#primaries = pickle.load(open('test_1024_primaries.pickle','rb'))
-primaries = pickle.load(open('1024_256_primaries_sorted_list.pickle','rb'))
+primaries = pickle.load(open('test_1024_primaries.pickle','rb'))
+#primaries = pickle.load(open('1024_256_primaries_sorted_list.pickle','rb'))
 primaries = primaries[:1024]
 
 el = 'abcdefghijklmnopqrstuvwxyz'
@@ -53,15 +53,15 @@ def score_cipher(cipher):
     print '\n\nTOTAL SCORE: ', total
 
 def decompose(cipher):
-    c = np.unpackbits(np.array(buffer(cipher)))[:26*26].reshape((26,26))
-    return np.where(c)
+    return np.where(cipher)
 
 def single_compatable(single):
     compatable = []
     incompatable = []
+    a, b = single
     for x in range(26):
         for y in range(x+1,26):
-            if single[0] != x and single[1] != y and single[0] != y and single[1] != x:
+            if (a != x and b != y and a != y and b != x) or (a == x and b == y):
                 compatable.append((x,y))
             else:
                 incompatable.append((x,y))
@@ -79,11 +79,40 @@ def single_compatable(single):
 #    score(cipher)
 #    print 'advertised score:',scr
 
+
+#returns the minimum score for a cipher (the sum of the primaries it contains)
+def min_score(c):
+    m_vec = np.zeros(p_dim[(0,8)].shape,dtype=bool) #default because a=i is common 
+    xs, ys = decompose(c)
+    for x,y in zip(xs,ys):
+        if x < y:
+            m_vec = np.logical_or(m_vec,p_dim[(x,y)])
+    m_vec = np.all(m_vec,axis=1)
+    return np.dot(s_vec,m_vec)
+
+def max_score(c):
+    xs, ys = decompose(c)
+    pairs = zip(xs,ys)
+    c_vec = single_ciphers_c[pairs[0]]
+    for x,y in pairs[1:]:
+        if x<y:
+            c_vec = np.logical_and(c_vec,single_ciphers_c[(x,y)])
+    return np.dot(c_vec,s_vec)
+
 def cipher_key(c):
     return str(np.packbits(c).data)
 
 def cipher_unkey(key):
     return np.unpackbits(np.array(buffer(key)))[:26*26].reshape((26,26)).astype(bool)
+
+def validCipher(c):
+    if np.any(np.diag(c)) or np.any(np.sum(c,axis=0)>1) or np.any(np.sum(c,axis=1)>1):
+        return False
+    return True
+
+def is_compatable(c1,c2):
+    c = np.logical_or(c1,c2)
+    return validCipher(c)
 
 max_primary_size = reduce(lambda p1, p2: max(p1, np.sum(cipher_unkey(p2[0]))/2), primaries, 0)
 
@@ -98,7 +127,7 @@ for x in range(26):
 #decompose primaries
 for index, p in enumerate(primaries):
     s_vec[index] = p[1][0]
-    xs,ys = decompose(p[0])
+    xs,ys = decompose(cipher_unkey(p[0]))
     num_pairings = len(xs)/2
     i = 0
     for x,y in zip(xs,ys):
@@ -112,10 +141,22 @@ single_ciphers_c = {}
 for single_cipher in single_ciphers_parent:
     single_ciphers_c[single_cipher] = np.copy(single_ciphers_parent[single_cipher])
     compat, incompat = single_compatable(single_cipher)
+    print '\n\n',single_cipher
+    print incompat
     for comp in compat:
         single_ciphers_c[single_cipher] = np.logical_or(single_ciphers_c[single_cipher],single_ciphers_parent[comp])  
     for inco in incompat:
         single_ciphers_c[single_cipher] = np.logical_and(single_ciphers_c[single_cipher],np.logical_not(single_ciphers_parent[inco]))
+
+#print 'checking compatibility vector'
+#for x,y in single_ciphers_c:
+#    c = np.zeros((26,26),dtype=bool)
+#    c[x][y] = True
+#    c[y][x] = True
+#    for index, (key, _) in enumerate(primaries):
+#        if single_ciphers_c[(x,y)][index] != is_compatable(c,cipher_unkey(key)):
+#            print 'PROBLEM!', x, y,single_ciphers_c[(x,y)][index], decompose(cipher_unkey(key))
+
 
 #first beam 
 beam = []
@@ -124,10 +165,9 @@ for x,y in single_ciphers_c:
     c[x][y] = True
     c[y][x] = True
     key = str(np.packbits(c).data)
-    beam.append((key,(np.packbits(single_ciphers_c[(x,y)]),np.dot(s_vec,single_ciphers_c[(x,y)]))))
+    beam.append((key,(np.packbits(c),(min_score(c),max_score(c)))))
 
 beam = sorted(beam, key=lambda x:x[1][1],reverse=True)
-
 
 def compatable(c):
     result = []
@@ -158,7 +198,7 @@ while not np.all(np.any(cipher_unkey(beam[0][0]),axis=0)):
     beam = beam[:beam_size(count)]
     count += 1
     next_beam = {}
-    for old_key, (comp,score) in beam:
+    for old_key, (comp,_) in beam:
         c = np.unpackbits(np.array(buffer(old_key)))[:26*26].reshape((26,26)).astype(bool)
         for x,y in compatable(c):
             new_c = np.copy(c)
@@ -166,10 +206,12 @@ while not np.all(np.any(cipher_unkey(beam[0][0]),axis=0)):
             new_c[y][x] = True
             new_key = str(np.packbits(new_c).data)
             if new_key not in next_beam:
-                new_comp = np.logical_and(np.unpackbits(comp),single_ciphers_c[(x,y)]) 
+                #new_comp = np.logical_and(np.unpackbits(comp),single_ciphers_c[(x,y)]) 
                 #next_beam[new_key] = (new_comp, np.sum(new_comp))
-                next_beam[new_key] = (np.packbits(new_comp), np.dot(s_vec,new_comp))
+                next_beam[new_key] = (min_score(new_c),max_score((new_c)))
+                #next_beam[new_key] = (np.packbits(new_comp), np.dot(s_vec,new_comp))
     beam = sorted(next_beam.items(),key=lambda x:x[1][1],reverse=True)
+    beam = sorted(beam,key=lambda x:x[1][0],reverse=True)
     print 'length of full beam:', len(beam)
     
 
